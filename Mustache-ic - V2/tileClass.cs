8e35303f336244b6ae26c7ae68e99100d6e63b87
Dominic <dominicacox@gmail.com>
2014-06-05 08:58:11 -0400

    First Commit

---
+++ Mustache-ic - V2/tileClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mustashe_ic
+{
+    class tileClass
+    {
+
+        public bool correctObject { get; set; }
+        public System.Windows.Forms.Button tile { get; set; }
+        
+
+        public tileClass()
+        {
+            correctObject = false;
+            tile = new System.Windows.Forms.Button();
+            tile.Click += new EventHandler(tile_clicked);
+        }
+
+        public tileClass(bool val)
+        {
+            correctObject = val;
+            tile = new System.Windows.Forms.Button();
+        }
+
+        public void refreshTile()
+        {
+            //change tile image
+            //sim fade in and out
+        }
+
+        public void hideTile()
+        {
+            this.tile.Hide();
+        }
+
+        private void tile_clicked(object sender, EventArgs e)
+        {
+            if(this.correctObject == true)
+            {
+                //Code to add mustache to button picture
+                //Add to score
+            }
+            else
+            {
+                //Code to remove points from score
+                //Remove lives
+            }
+
+            //code to fade in and fade out picture and change picture
+        }
+    }
+}
